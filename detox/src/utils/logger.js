f7b44f2443604e92988663708c5f17586c77976b
Yaroslav Serhieiev <noomorph@gmail.com>
2018-07-09 16:52:18 +0300

    code: removed wss level temporarily or permanently

--- detox/src/utils/logger.js
+++ detox/src/utils/logger.js
@@ -1,17 +1,22 @@
+const _ = require('lodash');
+const path = require('path');
 const winston = require('winston');
 const argparse = require('./argparse');
 
+const shouldRecordLogs = argparse.getArgValue('record-logs') !== 'none';
+const artifactsLocation = argparse.getArgValue('artifacts-location');
+
 const logger = winston.createLogger({
   level: argparse.getArgValue('loglevel') || 'info',
-  format: winston.format.json(),
-  transports: [
-    new winston.transports.File({
-      filename: 'detox-last.log',
-    }),
+  transports: _.compact([
+    shouldRecordLogs ? new winston.transports.File({
+      filename: path.join(artifactsLocation, 'detox-cli.log'),
+      format: winston.format.json(),
+    }) : null,
     new winston.transports.Console({
       format: winston.format.simple(),
     }),
-  ]
+  ]),
 });
 
 module.exports = logger;
